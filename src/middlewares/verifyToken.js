79ff16060379b082f74d184153bd76d09f37e551
dk-jaiswal-77 <dkjaiswal771999@gmail.com>
2022-05-24 20:37:36 +0545

    token verification modified

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next){
-    
-    const token = req.headers.Authorization && req.headers.Authorization.split(" ")[1];
 
-    jwt.verifyToken(token, process.env.ACCESS_TOKEN_SECRET_KEY, (error, user) => {
+    const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
+    // console.log(token);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (error, user) => {
         if(error){
             return res.json({status : false, error});
         }
